5d3cdcf4a50d123d07a75cd7c2ceef07d5efb693
LFF-Server <meerschweinchen5644@gmail.com>
2023-01-26 22:17:17 +0100

    make config

--- minecraftServer_runner.js
+++ minecraftServer_runner.js
@@ -1,43 +1,19 @@
 #!/usr/bin/env node
 const {execSync}=require("child_process");
-const serverDir="/home/lff/Programmes/MinecraftServer";
-const settings={
-	//restart:false,
-	run:true,
-	ram:"4G",
-	serverJar:"paper-1.12.2.jar",
-	javaPath:"/usr/lib/jvm/java-11-openjdk-amd64/bin/java",
-}
-const servers=[
-	{
-		folder:"BungeeCordServer",
-		serverJar:"BungeeCord.jar",
-		screenName:"minecraftServer_bungee",
-		ram:null,
-	},
-	{
-		folder:"minecraftServer_lobby",
-		ram:"2G",
-	},
-	{
-		folder:"minecraftServer_survival",
-	},
-	{
-		folder:"minecraftServer_creative",
-	},
-	{
-		folder:"minecraftServer_1.19.3_creative",
-		serverJar:"paper-1.19.3.jar",
-		screenName:"minecraftServer_1.19.3_creative",
-		javaPath:"java",
-		run:false,
-	},
-];
-process.chdir(serverDir);
-let server="";
+const {readFileSync}=require("fs");
+
+const config_servers="servers.json";
+const config_file="config.json";
+
+const servers=JSON.parse(readFileSync(config_servers,"utf-8"));
+const config=JSON.parse(readFileSync(config_file,"utf-8"));
+
+process.chdir(config.path||".");
+
+let server;
 for(server of servers){
 	server={
-		...settings,
+		...config.template_server,
 		...server,
 	};
 	if(!server.run){continue;}
